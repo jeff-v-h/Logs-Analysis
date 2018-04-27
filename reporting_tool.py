45e160c5a174b004dbfcf859840142d66332e91b
vagrant <vagrant@vagrant.vm>
2018-04-27 15:49:52 +0000

    Fix visual of python code

--- reporting_tool.py
+++ reporting_tool.py
@@ -8,22 +8,24 @@ db = psycopg2.connect("dbname=news")
 cursor = db.cursor()
 
 # Execute a query to fetch and print the 3 most popular articles of all time
-cursor.execute(
-    "SELECT title, count(*) as views from articles, log \
-    where log.path like CONCAT('%', articles.slug, '%') \
-    and (status = '200 OK') group by title \
-    order by views desc limit 3")
+cursor.execute("""
+    SELECT title, count(*) as views from articles, log
+    where log.path like CONCAT('%', articles.slug, '%')
+    and (status = '200 OK') group by title
+    order by views desc limit 3
+    """)
 results = cursor.fetchall()
 print "1. What are the most popular three articles of all time?"
 for result in results:
     print result[0] + " - " + str(result[1]) + " views"
 
 # Execute a query to fetch and print the authors ordered by most views
-cursor.execute(
-    "SELECT name, count(*) as views from articles, authors, log \
-    where log.path like CONCAT('%', articles.slug, '%') \
-    and articles.author = authors.id group by name \
-    order by views desc")
+cursor.execute("""
+    SELECT name, count(*) as views from articles, authors, log
+    where log.path like CONCAT('%', articles.slug, '%')
+    and articles.author = authors.id group by name
+    order by views desc
+    """)
 results = cursor.fetchall()
 print "2. Who are the most popular article authors of all time?"
 for result in results:
